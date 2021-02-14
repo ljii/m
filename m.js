979d6ba687da1cb975028ca1efde45b45c6fbada
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-02-14 14:21:43 +0530

    Update m.js

--- m.js
+++ m.js
@@ -19,9 +19,8 @@ if(!sessionStorage.ef&&new Date().getHours()<=6&&location.host!=='www.technewsgk
 // url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG
 // url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu
 
-if(location.host == "technicalgrowinc.blogspot.com"){
-    let jhdf = document.querySelector("meta[name='google-site-verification']")
-  jhdf.content="a-U3lvi7Y7GTifEqTf16lCW8CwOdub8hxuH2NWxaDp0"
-  }
-
+    let meta = document.createElement('meta');
+    meta.name = "google-site-verification";
+    meta.content = "cLnlQPK74cM93mcilUqTEnD3YR2-qaepYIwA7sfUedA";
+    document.getElementsByTagName('head')[0].appendChild(meta);
 
