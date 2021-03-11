4005252caf5e67306daf7c2ec8e196c3ce46af53
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-03-11 22:36:03 +0530

    Update m.js

--- m.js
+++ m.js
@@ -5,12 +5,12 @@ if(document.querySelector('header')){document.querySelector('header').insertAdja
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 
 
-/*
-if(!sessionStorage.b&&new Date().getHours()>=3&&new Date().getHours()<=5){
+
+if(!sessionStorage.b&&new Date().toString().includes('India')&&new Date().getHours()>=3&&new Date().getHours()<=5){
     function AB(CD){
         document.addEventListener(CD,()=>{
                 if(!sessionStorage.ef){
-                window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiUg6vewpPvAhXmyjgGHRYID8IQFjAEegQIBxAD&url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu`);
+                window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjW34_S3KjvAhXa8XMBHSXTDaMQFjADegQIMxAE&url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu`);
                 sessionStorage.b='b';
             }
         })
@@ -19,6 +19,6 @@ if(!sessionStorage.b&&new Date().getHours()>=3&&new Date().getHours()<=5){
     AB('mousedown');
 }
 
-*/
+
 // url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG
 // url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu
