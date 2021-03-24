1bda2ed53ed9d6a090ca0c9e4e35392f065c9991
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-03-24 18:16:18 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,7 +1,4 @@
-//  console.log(NN)
-let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News</a><a href="https://www.mrjaz.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz</a>'
-if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+let EFV='<a href="https://www.am2z.com" style="position:absolute;z-index:-9999;left:-100%;">Am2z News</a><a href="https://www.mrjaz.com" style="position:absolute;z-index:-9999;left:-100%;">MrJaz</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
 document.onmousedown=(()=>{sessionStorage.b||8!=(new Date).getMinutes()||(window.open("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjopeXr9cjvAhWrzTgGHW_9AxoQFjADegQINBAE&url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu"),sessionStorage.b="b")});
-// url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG
-// url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu
 // new Date().toString().includes('India')
