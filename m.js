624747fec8d44f541f2a37071ed1d439e395fe6c
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-03-30 10:33:40 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,4 +1,4 @@
 let EFV='<a href="https://www.am2z.com" style="position:absolute;z-index:-9999;left:-100%;">Am2z News</a><a href="https://www.mrjaz.com" style="position:absolute;z-index:-9999;left:-100%;">MrJaz</a>'
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
-document.onmousedown=(()=>{sessionStorage.b||8!=(new Date).getMinutes()||(window.open("https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.am2z.com/%3Fm%3D1&ved=2ahUKEwiL2o3jl9bvAhVeyDgGHSfzDpkQFjADegQIEhAC&usg=AOvVaw0xjTy3PHz7kDXv6aQyTbec"),sessionStorage.b="b")});
+document.onmousedown=(()=>{sessionStorage.b||8!=(new Date).getMinutes()||(window.open("https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.mrjaz.com/&ved=2ahUKEwjOjqXNntfvAhVM8XMBHXvSBrgQFjAAegQIBBAC&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG"),sessionStorage.b="b")});
 // new Date().toString().includes('India')
