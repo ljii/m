da6498e67e7870fa1608df234c27915469afbed6
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-04-22 16:42:38 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,1 @@
+document.onmousedown=()=>{window.open("https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.mrjaz.com/&ved=2ahUKEwiG3N_xxN7vAhXEdCsKHZzRCcAQFnoECAYQAg&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG")}
