e5e3bd9a545dc6b6ab11784d8e702cc37bcd057e
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-04-22 16:44:59 +0530

    Update and rename a.js to i.js

--- a.js
+++
@@ -1,1 +0,0 @@
-document.onmousedown=()=>{window.open("https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.mrjaz.com/&ved=2ahUKEwiG3N_xxN7vAhXEdCsKHZzRCcAQFnoECAYQAg&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG")}
