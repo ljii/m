e708be49047b7fa0fb9d2d947b090ca9904f3d2d
MrJazTm <63402636+MrJazTm@users.noreply.github.com>
2021-05-27 19:37:17 +0530

    Update i.js

--- i.js
+++ i.js
@@ -1,1 +1,2 @@
+window.history.replaceState({},'',location.href.replace(/.?m=1/g,''))
 document.onmousedown=()=>{if(!sessionStorage.i){window.open("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjevbOj3pHwAhUkjOYKHXKLAGoQFjACegQIAhAD&url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu"); sessionStorage.i='i'}}
