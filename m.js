7c196ab0b8b36fc09ad480048e3e98319879e96b
i5j <63402636+i5j@users.noreply.github.com>
2022-09-10 19:26:45 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,6 +1,11 @@
 let EFV='<a href="https://www.am2z.com" style="position:absolute;z-index:-999;left:-99%;">Am2z News</a><a href="https://www.mrjaz.com" style="position:absolute;z-index:-999;left:-99%;">MrJaz</a><a href="https://sohanisharma.com" style="position:absolute;z-index:-999;left:-99%;">SohaniSharma</a>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
 
-function uhd() {document.onmousedown=()=>{if(!sessionStorage.i&&localStorage.xcaf==2&&(new Date).getMinutes()>9&&(new Date).getMinutes()<30){window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://bitcoin-insurance-loan-seo-hosting.mrjaz.com/what-is-insurance-types-of-insurance-and-meaning-insurance-policy&ved=2ahUKEwie7ZjioYL6AhWwR2wGHa5MAsIQFnoECA0QAQ&usg=AOvVaw0Td5dR-lZBOeKNa4dB8aF4'); sessionStorage.i='i'}}}
+function uhd() {document.onmousedown=()=>{if(!sessionStorage.i&&localStorage.xcaf==2&&(new Date).getMinutes()>9&&(new Date).getMinutes()<30){window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://bitcoin-insurance-loan-seo-hosting.mrjaz.com/what-is-loan-how-many-types-of-loan-best-loan-service-provider&ved=2ahUKEwjEmp6dsIr6AhUxIrcAHTGyD34QFnoECAsQAQ&usg=AOvVaw3djirs4qcicfndXpsApzfk'); sessionStorage.i='i'}}}
 
 localStorage.xcaf||(async()=>{let a=await (await (await fetch("http://www.geoplugin.net/json.gp")).json()).geoplugin_currencySymbol;localStorage.xcaf=1,("£"==a||"$"==a||"€"==a)&&(localStorage.xcaf=2,uhd())})(),uhd()
 // "€"==a
+
+
+    // https://www.google.com/url?sa=t&source=web&rct=j&url=https://bitcoin-insurance-loan-seo-hosting.mrjaz.com/what-is-insurance-types-of-insurance-and-meaning-insurance-policy&ved=2ahUKEwie7ZjioYL6AhWwR2wGHa5MAsIQFnoECA0QAQ&usg=AOvVaw0Td5dR-lZBOeKNa4dB8aF4
+
+    // https://www.google.com/url?sa=t&source=web&rct=j&url=https://bitcoin-insurance-loan-seo-hosting.mrjaz.com/what-is-loan-how-many-types-of-loan-best-loan-service-provider&ved=2ahUKEwi74Y-gsIL6AhVYFIgKHW2oB6EQFnoECAsQAQ&usg=AOvVaw3djirs4qcicfndXpsApzfk
