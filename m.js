ba26c76c510b2c6a8647407e8e965e916858362a
i5j <63402636+i5j@users.noreply.github.com>
2022-10-11 13:50:00 +0530

    Update m.js

--- m.js
+++ m.js
@@ -4,7 +4,7 @@ if (!localStorage.xcaf){
   (async()=>{
     let aa=await(await(await fetch(`http://www.geoplugin.net/json.gp`)).json());
     localStorage.xcaf=1;
-    if(/India|United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|Brazil|New Zealand|Italy|Luxembourg|Finland/.test(aa.geoplugin_countryName)){localStorage.xcaf=2}
+    if(/United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|Brazil|New Zealand|Italy|Luxembourg|Finland/.test(aa.geoplugin_countryName)){localStorage.xcaf=2}
                                    
 //     if(/£|[$]|€/.test(aa.geoplugin_currencySymbol)){localStorage.xcaf=2;
 //     if(/Spain/.test(aa.geoplugin_countryName)){localStorage.xcaf=3}
