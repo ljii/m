308965d41d6603d36e212cfe0432b002524a4d35
i5j <63402636+i5j@users.noreply.github.com>
2022-11-03 11:20:02 +0530

    Update m.js

--- m.js
+++ m.js
@@ -20,7 +20,7 @@ function uhd(){
   document.onmousedown=()=>{
     let dte=(new Date).getSeconds();
     (async()=>{try{await import('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
-        if(!sessionStorage.i&&(localStorage.gieu==2&&dte>9&&dte<12||localStorage.gieu==3&&dte>9&&dte<11)){
+        if(!sessionStorage.i&&(localStorage.gieu==2&&dte>9&&dte<11||localStorage.gieu==3&&dte>9&&dte<11&&false)){
             window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj26OjulpH7AhXbcGwGHcAxAcE4PBAWegQIIhAB&url=https%3A%2F%2Fwww.am2z.com%2F2021%2F10%2Fblockchain-kya-hai-blockchain-kaise-Kaam-karta-hai.html&usg=AOvVaw3LmyExZICME6lLy5o-o0_8'); 
             sessionStorage.i='i';
         }         
