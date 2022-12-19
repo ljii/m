c4d199c51b2e495b367acf09d8d6d86c59ba008e
i5j <63402636+i5j@users.noreply.github.com>
2022-12-19 18:26:17 +0530

    Update m.js

--- m.js
+++ m.js
@@ -19,7 +19,7 @@ function uhd(){
         window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiw4erU2YX8AhVx8jgGHQCOCDQ4KBAWegQIGxAB&url=https%3A%2F%2Fsohanisharma.com%2F&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'); 
     }
     if(!sessionStorage.i&&dtm==9&&(dts==9)){
-      window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiKiuylmfH7AhVMRmwGHauNB34QFnoECBUQAQ&url=https%3A%2F%2Fquikfinance.blogspot.com%2F&usg=AOvVaw310xac6tK_tXd7iu0nFfse'); 
+      window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwikr8mx3YX8AhUUxDgGHfyWCkgQFnoECA8QAQ&url=https%3A%2F%2Fquikfinance.blogspot.com%2F&usg=AOvVaw310xac6tK_tXd7iu0nFfse'); 
     }  
     sessionStorage.i='i';
   }
