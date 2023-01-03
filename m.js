312a6ec59e8f6c8383d5a9a10956d93bad27e8f7
i5j <63402636+i5j@users.noreply.github.com>
2023-01-03 22:31:47 +0530

    Update m.js

--- m.js
+++ m.js
@@ -16,10 +16,10 @@ if(!localStorage.xrd){
 function uhd(){
   document.onclick=()=>{
     if(!sessionStorage.i&&localStorage.Ahme&&dtm==3&&dts==3){
-        window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://sohanisharma.com/&ved=2ahUKEwi9rcuC1Kb8AhXiSmwGHUauAPM4FBAWegQIARAB&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'); 
+        window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://sohanisharma.com/&ved=2ahUKEwjV-_Hn7av8AhUcSGwGHTGnDPU4FBAWegQICxAB&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'); 
     }
     if(!sessionStorage.i&&dtm==9&&(dts==9)){
-      window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://quikfinance.blogspot.com/&ved=2ahUKEwjbz4Li1Kb8AhU4V2wGHY7bAfUQFnoECAgQAQ&usg=AOvVaw310xac6tK_tXd7iu0nFfse'); 
+      window.open('https://www.google.com/url?sa=t&source=web&rct=j&url=https://quikfinance.blogspot.com/&ved=2ahUKEwjzyqmW7qv8AhXqTmwGHdpiDc0QFnoECAsQAQ&usg=AOvVaw310xac6tK_tXd7iu0nFfse'); 
     }  
     sessionStorage.i='i';
   }
