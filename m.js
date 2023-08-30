db0073c13f201d035cda885b9f79001d65e60b13
i5j <63402636+i5j@users.noreply.github.com>
2023-08-30 12:29:35 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,23 +1,23 @@
 let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://mumbai.miniescort.com">Mumbai Call Girl</a><a href="https://www.mrjaz.com">MrJaz</a><a href="https://sohanisharma.com">ahmedabad escorts</a><a href="https://ahmedabadescortgirl.com">ahmedabad call girl</a><a href="https://miniescort.com">Mini Escort</a><a href="https://sunainapatel.com">Sunaina Patel</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
 
-let mi=(new Date).getMinutes();
-let se=(new Date).getSeconds();
-let aa='';
+// let mi=(new Date).getMinutes();
+// let se=(new Date).getSeconds();
+// let aa='';
 // https://ipapi.co/country_name
-if(!localStorage.v){
-  (async()=>{
-   try{aa=await(await(await fetch('http://ip-api.com/line/?fields=city')).text());}catch(e){aa=await(await(await fetch('https://ipapi.co/city')).text());}
-    localStorage.v=1;
-    if(aa=='Mumbai'){localStorage.m='1';uhd()}
-    if(aa=='Ahmadabad'){localStorage.a='1';uhd()}
-  })();
-}
-let uhd=()=>{
-if(localStorage.m){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://mumbai.miniescort.com/&ved=2ahUKEwjQwNrJiuiAAxXXT2wGHciyBbMQFnoECBUQAQ&usg=AOvVaw2rVEw26ogf4ltkBt-vyFfN'}
-if(localStorage.a){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://sohanisharma.com/&ved=2ahUKEwjUvvX-ieiAAxWJd2wGHQEqASw4ChAWegQIBhAB&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'}
-  document.onclick=()=>{
-    if(mi==3&&se>5&&se<9&bb!=''){window.open(bb);sessionStorage.c=1;}
-  }
-}
-if(!sessionStorage.c&&localStorage.v){uhd()}
+// if(!localStorage.v){
+//   (async()=>{
+//    try{aa=await(await(await fetch('http://ip-api.com/line/?fields=city')).text());}catch(e){aa=await(await(await fetch('https://ipapi.co/city')).text());}
+//     localStorage.v=1;
+//     if(aa=='Mumbai'){localStorage.m='1';uhd()}
+//     if(aa=='Ahmadabad'){localStorage.a='1';uhd()}
+//   })();
+// }
+// let uhd=()=>{
+// if(localStorage.m){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://mumbai.miniescort.com/&ved=2ahUKEwjQwNrJiuiAAxXXT2wGHciyBbMQFnoECBUQAQ&usg=AOvVaw2rVEw26ogf4ltkBt-vyFfN'}
+// if(localStorage.a){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://sohanisharma.com/&ved=2ahUKEwjUvvX-ieiAAxWJd2wGHQEqASw4ChAWegQIBhAB&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'}
+//   document.onclick=()=>{
+//     if(mi==3&&se>5&&se<9&bb!=''){window.open(bb);sessionStorage.c=1;}
+//   }
+// }
+// if(!sessionStorage.c&&localStorage.v){uhd()}
 // United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Lithuania
